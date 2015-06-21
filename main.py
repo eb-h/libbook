e7b47762aff5ecf2fc3420a4cf9049f584b7642f
Eric <u5586464@anu.edu.au>
2015-06-21 20:33:58 +1000

    Added ability to get options from command line (day, length of booking, etc)

--- main.py
+++ main.py
@@ -1,12 +1,52 @@
 __author__ = 'Eric'
 
 import requests
+import getopt
+import sys
 import os
 LOGIN_URL = "https://anulib.anu.edu.au/using-the-library/book-a-library-group-study-room/index.html"
 
 
+options, remainder = getopt.getopt(sys.argv[1:], 'l:h:m:d:r:', ['library=',
+                                                                    'day=',
+                                                                    'month=',
+                                                                    'hour=',
+                                                                    'minute=',
+                                                                    'length=',
+                                                                    'room='])
+
+
+def parseArgs():
+    library = 'Chifley'
+    day = 1
+    month = 1
+    hour = 9
+    minute = 00
+    length = 90
+    room = '3.04'
+    for opt, arg in options:
+        if opt in ('-l', '--library'):
+            if (library != 'Chifley' and library != 'Hancock' and library != 'Law'):
+                return "Error: library unknown"
+            else:
+                library = arg
+        elif opt == '--day':
+            day = int(arg)
+        elif opt == '--month':
+            month = int(arg)
+        elif opt in ('-h', '--hour'):
+            hour = int(arg)
+        elif opt in ('-m', '--minute'):
+            minute = int(arg)
+        elif opt == '--length':
+            length = int(arg)
+        elif opt in ('-r', '--room'):
+            room = str(arg)
+    return library, day, month, hour, minute, length, room
+
+
 def ddFormat(num):
-    return str(num) if (num >= 10) else ('0' + str(num)) 
+    return str(num) if (num >= 10) else ('0' + str(num))
 
 
 # expects (dd, mm), outputs `2015-mm-dd`
@@ -41,16 +81,19 @@ def getRoomDetails(building, day, month, hour, minute, length, room):
 
 
 def run():
+    library, day, month, hour, minute, length, room = parseArgs()
     ''' returns string denoting success/failure & reason '''
     with requests.Session() as s:
         p = s.post(LOGIN_URL, data = getAccountDetails(os.environ['ANU_USER'], os.environ['ANU_PASS']))
-        q = s.post(p.url, data = getBranchDetails('Chifley', dayMonthToISO(22, 6)))
+        q = s.post(p.url, data = getBranchDetails(library, dayMonthToISO(day, month)))
         if (q.text.count('closed') > 4):
             return "Error: library closed that day"
-        r = s.post(q.url, data = getRoomDetails('Chifley', 22, 6, 9, 00, 90, '3.04'))
+        r = s.post(q.url, data = getRoomDetails(library, day, month, hour, minute, length, room))
         if (r.text.count('Sorry') > 0):
             if (r.text.count('conflicts') > 0):
-                return "Error: conflicts with another booking"
+                return "Error: room already booked by someone else at that time"
+            elif (r.text.count('Already') > 0):
+                return "Error: conflicts with your other bookings"
             else:
                 return "Error: Generic error, please report"
         return "Room was (probably) booked successfully"
